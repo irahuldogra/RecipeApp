aacc6e899ac502bffa2381a0e814464491aa84f6
Rahul Dogra <rahuldogra1998@gmail.com>
2020-09-23 23:44:39 +0530

    Random Recipe Script

--- script.js
+++ script.js
@@ -15,17 +15,38 @@ async function getRandomMeal() {
 }
 
 async function getMealById(id) {
-  const meal = await fetch(
+  const resp = await fetch(
     "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
   );
+
+  return meal;
 }
 
-async function getMealsBySearch(term) {
-  const meals = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
-  );
-}
+function addMeal(mealData, random = false) {
+  console.log(mealData);
 
-addMeal(mealData, random= false){
-    
+  const meal = document.createElement("div");
+  meal.classList.add("meal");
+
+  meal.innerHTML = `
+      <div class="meal-header">
+          ${
+            random
+              ? `
+          <span class="random"> Random Recipe </span>`
+              : ""
+          }
+          <img
+              src="${mealData.strMealThumb}"
+              alt="${mealData.strMeal}"
+          />
+      </div>
+      <div class="meal-body">
+          <h4>${mealData.strMeal}</h4>
+          <button class="fav-btn">
+              <i class="fas fa-heart"></i>
+          </button>
+      </div>
+  `;
+  meals.appendChild(meal);
 }
