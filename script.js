ba79e400fc9e7349a25c54abccd47c3e0a94639d
Rahul Dogra <rahuldogra1998@gmail.com>
2020-09-23 20:27:41 +0530

    git commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const meals = document.getElementById("meals");
+
+getRandomMeal();
+
+async function getRandomMeal() {
+  const resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  const respData = await resp.json();
+  const randomMeal = respData.meals[0];
+
+  console.log(respData.meals[0]); // Before Meals was inside an object
+
+  addMeal(randomMeal, true);
+}
+
+async function getMealById(id) {
+  const meal = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+  );
+}
+
+async function getMealsBySearch(term) {
+  const meals = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
+  );
+}
+
+addMeal(mealData, random= false){
+    
+}
